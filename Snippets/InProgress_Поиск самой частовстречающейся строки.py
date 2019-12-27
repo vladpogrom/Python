fe56c4c1a2d259df7c4d76cbebe6d0dd82a0fd26
Vladislav <47423111+vladpogrom@users.noreply.github.com>
2019-12-27 16:17:35 +0500

    Update InProgress_Поиск самой частовстречающейся строки.py

--- Snippets/InProgress_Поиск самой частовстречающейся строки.py
+++ Snippets/InProgress_Поиск самой частовстречающейся строки.py
@@ -1,13 +1,10 @@
-```
-Читаем строки в одну строку(str_in), приводим это к множеству (mn)
-Считываем первое значение множества = (word) и считаем count = (count_max)
-Затем циклом записываем значение из множества в переменную(x[i]) и считаем str_in.count(x)
-Если x[i] > count_max, то count_max = x[i], word = x
-Если x[i] = count_max, то 
-  Если x > word, то wor1 = x, иначе continiue
-В конце выводим print(word, count_max)
-
-```
+#Читаем строки в одну строку(str_in), приводим это к множеству (mn)
+#Считываем первое значение множества = (word) и считаем count = (count_max)
+#Затем циклом записываем значение из множества в переменную(x[i]) и считаем str_in.count(x)
+#Если x[i] > count_max, то count_max = x[i], word = x
+#Если x[i] = count_max, то 
+  #Если x > word, то wor1 = x, иначе continiue
+#В конце выводим print(word, count_max)
 
 #чтение нескольких строк
 l=inf.read().split() 
