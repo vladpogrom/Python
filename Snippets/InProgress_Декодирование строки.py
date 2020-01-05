e5711936f9a0efad64c12cba9feea22f5035586a
Vladislav <47423111+vladpogrom@users.noreply.github.com>
2020-01-05 13:25:01 +0500

    Update InProgress_Декодирование строки.py

--- Snippets/InProgress_Декодирование строки.py
+++ Snippets/InProgress_Декодирование строки.py
@@ -25,6 +25,7 @@ for i in range(len(a)):
 #Еще как вариант можно накапливать все цифры в переменную строчного типа, пока не встретим букву. потом приводим эту строку к int
 #Выводим
 
+#Поиск чисел в строке через while
 s = input()
 l = len(s)
 integ = []
@@ -44,6 +45,23 @@ while i < l:
         integ.append(int(s_int))
 
 print(integ)
+
+#Поиск чисел в строке через if
+line = input()
+num_out_of_line = []
+str_num = ""
+ 
+for i in range(len(line)):
+    if line[i] in "0123456789":
+        str_num += line[i]
+        try:
+            if line[i+1] not in "0123456789":
+                num_out_of_line.append(str_num)
+                str_num = ""
+        except:
+            num_out_of_line.append(str_num)
+ 
+print(list(map(int, num_out_of_line)))
 
 #для справки
 S.isdigit() #Состоит ли строка из цифр
